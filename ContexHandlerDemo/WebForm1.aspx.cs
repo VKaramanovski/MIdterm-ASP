b7626b715f13cf9eb56792ebdc017b37e809fa6f
vladimir <vladimir@VLADIMIRKAR0EF0>
2019-11-14 19:26:01 +0100

    Add project files.

---
+++ ContexHandlerDemo/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ContexHandlerDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public string Name
+        {
+            get
+            {
+                return txtName.Text;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return txtEmail.Text;
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("~/WebForm2.aspx");
+        }
+    }
+}
